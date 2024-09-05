35f19a64c408e2a7181ba8afeab161e5aeeca262
jay6697117 <871506263@qq.com>
2024-09-06 07:48:08 +0800

    init

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,6 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = `<h1>${JSON.stringify(ctx.query)}</h1>`;
-
   }
   // 获取用户信息
   async user() {
@@ -13,6 +12,16 @@ class HomeController extends Controller {
     <h1 style="color:red;">${JSON.stringify(ctx.params)}</h1>
     <h3>${JSON.stringify(ctx.query)}</h3>
     </div>`;
+  }
+
+  // post 请求方法
+  async add() {
+    const { ctx } = this;
+    const { title } = ctx.request.body;
+    // Egg 框架内置了 bodyParser 中间件来对 POST 请求 body 解析成 object 挂载到 ctx.request.body 上
+    ctx.body = {
+      title
+    };
   }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -21,6 +21,15 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  // config.security = {
+  //   csrf: {
+  //     enable: false,
+  //     ignoreJSON: true
+  //   },
+  //   domainWhiteList: [ '*' ], // 配置白名单
+  // };
+
+
   return {
     ...config,
     ...userConfig,
